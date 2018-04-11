b7253fe0609e778c513ddeca844ec9381621197e
ge1o4 <tatarin.com@gmail.com>
2018-04-11 20:21:45 +0300

    remove old code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { User } from './models/User';
-
-import { USERS } from './users-mock';
-import { UsersService } from './users.service';
-import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
-import 'rxjs/add/operator/switchMap';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  currentUser: User
-  users$: Observable<User[]>
-  subscribtion: Subscription
-
-  constructor(private usersService: UsersService){
-
-  }
-
-  ngOnInit(){
-    this.getUsers();
-
-    // this.subscribtion = this.usersService.getList().subscribe((x) => console.log(x));
-  }
-
-  ngOnDestroy(){
-    // this.subscribtion.unsubscribe();
-  }
-
-  getUsers(){
-    this.users$ = this.usersService.getList();
-  }
-
-  onSelect(user: User){
-    this.currentUser = user;
-  }
-
-  onDeleteUser(user: User){
-    this.users$ = this.usersService
-      .deleteUser(user)
-      .switchMap(() => this.usersService.getList())
-      ;
-    this.currentUser = null;
-  }
+export class AppComponent {
+  constructor(){}  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import { DetailsComponent } from './details/details.component';
-import { UsersService } from './users.service';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 const routes: Route[] = [
-  { path: '', redirectTo: 'list', pathMatch: 'full'}, // default route
-  { path: 'list', component: ListComponent},
+  // { path: '', redirectTo: 'list', pathMatch: 'full'}, // default route
+  // { path: 'list', component: ListComponent},
   { path: '**', component: NotFoundComponent} // Not found route
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
-    DetailsComponent,
     NotFoundComponent,
     NavigationComponent,
   ],
@@ -27,7 +22,6 @@ const routes: Route[] = [
     BrowserModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
